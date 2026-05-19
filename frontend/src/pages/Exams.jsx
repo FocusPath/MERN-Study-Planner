725fee48b25f8437f00ea74718367a84ed379d2b
VinuliR <219432275+VinuliR@users.noreply.github.com>
2026-05-20 03:49:37 +0530

    added topics (adding and deleting), exams can be viewed for checklist, elements in checklist can be clicked on to check and progress is calculated accordingly

--- frontend/src/pages/Exams.jsx
+++ frontend/src/pages/Exams.jsx
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar.jsx'
 import Card from '../components/Card.jsx'
+import CheckBox from '../components/CheckBox.jsx'
 import { useState } from 'react'
 
 // test data
@@ -8,28 +9,20 @@ const examsTest = [
 		id: 1,
 		title: 'Physics',
 		date: new Date('2026-05-11T10:30:00'),
-		topics: ['Oscillations and Waves',
-			'Gravitational Field',
-			'Electronics',
-			'Electromagnetism'],
-		isTopicDone: [1, 0, 1, 0],
+		topics: [{ topic: "Oscillations and Waves", done: true }, { topic: "Gravitational Field", done: true }, { topic: "Electronics", done: true }, { topic: "Electromagnetism", done: false }]
 	},
 	{
 		id: 2,
 		title: 'Chemistry',
-		date: new Date('2026-05-11T10:30:00'),
-		topics: [
-			'Organic Chemistry',
-			'Thermochemistry',
-			'Electrochemistry'
-		],
-		isTopicDone: [1, 1, 0]
+		date: new Date('2026-05-12T10:30:00'),
+		topics: [{ topic: "Organic Chemistry", done: true }, { topic: "Thermochemistry", done: false }, { topic: "Electrochemistry", done: false }, { topic: "Mole Calculation", done: false }]
 	},
 ]
 
 const FormTypes = {
 	ADD_EXAM: 'add',
 	EDIT_EXAM: 'changeName',
+	VIEW_EXAM: 'viewExam'
 }
 
 //https://www.w3schools.com/react/react_forms.asp
@@ -43,21 +36,27 @@ const Exams = () => {
 	const [examName, setExamName] = useState('')
 	const [examDate, setExamDate] = useState('')
 
+	const [topicName, setTopicName] = useState('')
+	const [examTopics, setExamTopics] = useState([])
 
-	// keeps track of current exam being edited
-	const [editId, setEditId] = useState(null)
+	// keeps track of current exam being edited/viewed
+	const [selectedId, setSelectedId] = useState(null)
 
 	// when a form should appear
 	const [showForm, setShowForm] = useState(false);
 	// what type of form sould appear
 	const [typeForm, setTypeForm] = useState(null)
 
-	function calculateProgress(isTopicDone) { //https://stackoverflow.com/questions/1230233/how-to-find-the-sum-of-an-array-of-numbers
-		const sum = isTopicDone.reduce((partialSum, a) => partialSum + a, 0);
-		if (isTopicDone.length === 0) {
-			return 0
+	const selectedExam = exams.find(exam => exam.id === selectedId);
+
+	function calculateProgress(topics) { //https://stackoverflow.com/questions/1230233/how-to-find-the-sum-of-an-array-of-numbers
+		const sum = topics.reduce(
+			(count, item) => count + (item.done ? 1 : 0), 0
+		);
+		if (topics.length === 0) {
+			return 0;
 		}
-		return (sum / isTopicDone.length * 100).toFixed(1)
+		return (sum / topics.length * 100).toFixed(1)
 	}
 
 	function deleteExam(id) {
@@ -65,50 +64,42 @@ const Exams = () => {
 
 	}
 
-	function newID() {
-		const newId = exams.length + 1;
-		return newId;
-	}
-
-	function editingTopics() {
-
-	}
-
 	function addExam() {
-		// validate inputs
 		if (!examName.trim()) {
-			alert('Please enter exam name')
-			return
+			alert('Please enter exam name');
+			return;
 		}
 		if (!examDate.trim()) {
-			alert('Please enter exam date')
-			return
+			alert('Please enter exam date');
+			return;
 		}
-
+		// Optionally: require at least one topic
+		if (examTopics.length === 0) {
+			alert('Please add at least one topic');
+			return;
+		}
 		const newExam = {
-			id: newID(),
+			id: Date.now(),
 			title: examName,
 			date: new Date(examDate),
-			topics: [],
-			isTopicDone: []
-		}
-
-		// prev gets latest state
-		setExams(prev => [...prev, newExam])
-
-		// reset tracking variables
+			topics: examTopics,
+		};
+		setExams(prev => [...prev, newExam]);
 		resetInputs();
 	}
+
 
 	function resetInputs() {
-		setExamName('')
-		setExamDate('')
-		setEditId(null)
-		setShowForm(false)
-		setTypeForm(null)
+		setExamName('');
+		setExamDate('');
+		setSelectedId(null);
+		setShowForm(false);
+		setTypeForm(null);
+		setTopicName('');
+		setExamTopics([]);
 	}
 
-	function editExam(exam) {
+	function editExam() {
 		// validate inputs
 		if (!examName.trim()) {
 			alert('Please enter exam name')
@@ -119,24 +110,62 @@ const Exams = () => {
 			return
 		}
 
-		const newExam = {
-			id: newID(),
-			title: examName,
-			date: new Date(examDate),
-			topics: [],
-			isTopicDone: []
+		if (examTopics.length === 0) {
+			alert('Please add at least one topic');
+			return;
 		}
 
-		// prev gets latest state
-		setExams(prev => [...prev, newExam])
+		setExams(prev =>
+			prev.map(exam =>
+				exam.id === selectedId
+					? { ...exam, title: examName, date: new Date(examDate),topics: examTopics }
+
+					: exam
+			)
+		)
 
 		// reset tracking variables
 		resetInputs();
 	}
 
-	// function editTopicCheck(isTopicDone) {
-	// 	setExams()
-	// }
+	function toggleTopic(index) { setExams(prev => prev.map(exam => exam.id === selectedId ? { ...exam, topics: exam.topics.map((topic, i) => i == index ? { ...topic, done: !topic.done } : topic) } : exam)) }
+
+	function deleteTopic(index) {
+		setExams(prev =>
+			prev.map(exam =>
+				exam.id === selectedId
+					? {
+						...exam,
+						topics: exam.topics.filter((_, i) => i !== index)
+					}
+					: exam
+			)
+		)
+	}
+
+	function addTopic() {
+		if (!topicName.trim()) {
+			alert('Please enter a topic name');
+			return;
+		}
+		const newTopic = {
+			topic: topicName,
+			done: false,
+		};
+		setExamTopics((prev) => [...prev, newTopic]);
+		setTopicName('');
+	}
+
+	const CancelButton = ({ onClick }) => {
+		return (
+			<button
+				onClick={onClick}
+				className="rounded bg-red-500 px-2 py-1 text-white hover:bg-red-600"
+			>
+				Cancel
+			</button>
+		)
+	}
 
 	return (
 		<div className="flex min-h-screen bg-black text-white">
@@ -154,13 +183,21 @@ const Exams = () => {
 									<Card
 										color={'blue'}
 										title={`${item.title}`}
-										caption={`${item.date.toLocaleString()}`}
-										caption={`${calculateProgress(item.isTopicDone)}%`}
+										caption={`${item.date.toLocaleString()} || ${calculateProgress(item.topics)}%`}
 									/>
 
 
 									<button onClick={() => {
-										//editExam(item)
+										setShowForm(true), setTypeForm(FormTypes.VIEW_EXAM), setSelectedId(item.id)
+									}}
+										className="rounded bg-green-500 px-2 py-1 text-xs text-white hover:bg-green-600"
+									>
+										View
+									</button>
+
+									<button onClick={() => {
+										// setShowForm(true), setTypeForm(FormTypes.EDIT_EXAM), setExamDate(item.date.toLocaleDateString()), setExamName(item.title), setSelectedId(item.id)
+										setShowForm(true), setTypeForm(FormTypes.EDIT_EXAM), setExamDate(item.date.toLocaleDateString()), setExamName(item.title), setSelectedId(item.id) ,setExamTopics(item.topics)
 									}}
 										className="rounded bg-blue-500 px-2 py-1 text-xs text-white hover:bg-blue-600"
 									>
@@ -174,17 +211,12 @@ const Exams = () => {
 										Delete
 									</button>
 
-
-
-
-
 								</div>
 
 							))
 						}
 					</div>
 				</section>
-
 			</main>
 
 
@@ -200,7 +232,7 @@ const Exams = () => {
 			{/* comparison uses === instead of == */}
 			{showForm && typeForm === FormTypes.ADD_EXAM && (
 				<div className="fixed inset-0 flex items-center justify-center bg-black/70">
-					<div className="w-full max-w-md rounded-xl bg-white p-6 text-black shadow-2xl">
+					<div className="w-full max-w-md max-h-[90vh] overflow-y rounded-xl bg-white p-6 text-black shadow-2xl">
 						<h2 className="mb-4 text-2xl font-semibold">
 							ADD EXAM!
 						</h2>
@@ -216,6 +248,28 @@ const Exams = () => {
 							placeholder='Enter Exam date'
 							value={examDate}
 							onChange={(e) => setExamDate(e.target.value)} />
+						<hr className="my-2 border-gray-500" />
+						<h1 className="mb-4 text-2xl font-semibold">
+							ADD TOPICS!
+						</h1>
+						<div className="flex gap-2">
+							<input
+								className="flex-1 rounded border p-2"
+								type="text"
+								placeholder="New topic"
+								value={topicName}
+								onChange={e => setTopicName(e.target.value)}
+							/>
+							<button onClick={addTopic}>Add</button>
+						</div>
+						<div className="flex gap-2">
+							{examTopics.map((topic, index) => (
+								<div key={index} className="flex items-center gap-2 rounded-lg border p-2">
+									<div>{topic.topic}</div>
+									<button onClick={() => setExamTopics(examTopics.filter((_, i) => i !== index))}>Delete</button>
+								</div>
+							))}
+						</div>
 
 						<button onClick={() => {
 							addExam()
@@ -225,13 +279,102 @@ const Exams = () => {
 							Submit
 						</button>
 
-						<button
-							onClick={() => resetInputs()}
-							className="rounded bg-red-500 px-2 py-1 text-xs text-white hover:bg-red-600"
+						<CancelButton onClick={resetInputs} />
+
+					</div>
+
+				</div>
+
+			)}
+
+			{/* floating form for editing exam */}
+			{showForm && typeForm === FormTypes.EDIT_EXAM && (
+				<div className="fixed inset-0 flex items-center justify-center bg-black/70">
+					<div className="w-full max-w-md max-h-[90vh] overflow-y rounded-xl bg-white p-6 text-black shadow-2xl">
+						<h2 className="mb-4 text-2xl font-semibold">
+							EDIT EXAM!
+						</h2>
+
+						<input className="w-full rounded-lg border border-gray-300 p-3 outline-none focus:border-black"
+							type="text"
+							placeholder='Enter Exam name'
+							value={examName}
+							onChange={(e) => setExamName(e.target.value)} />
+
+						<input className="w-full rounded-lg border border-gray-300 p-3 outline-none focus:border-black"
+							type="date"
+							placeholder='Enter Exam date'
+							value={examDate}
+							onChange={(e) => setExamDate(e.target.value)} />
+
+						<hr className="my-2 border-gray-500" />
+						<h1 className="mb-4 text-2xl font-semibold">
+							EDIT TOPICS!
+						</h1>
+
+						<div className="flex gap-2">
+							<input
+								className="flex-1 rounded border p-2"
+								type="text"
+								placeholder="New topic"
+								value={topicName}
+								onChange={e => setTopicName(e.target.value)}
+							/>
+							<button onClick={addTopic} className="rounded bg-gray-600 px-2 py-1 text-white hover:bg-gray-800">Add</button>
+						</div>
+						<div className="flex flex-col gap-2 mt-2">
+  {examTopics.map((topic, index) => (
+    <div key={index} className="flex gap-2">
+      <div className="flex-1 rounded border p-2">{topic.topic}</div>
+      <button
+        onClick={() => setExamTopics(examTopics.filter((_, i) => i !== index))}
+        className="rounded bg-red-500 px-2 py-1 text-white hover:bg-red-600"
+      >
+        Delete
+      </button>
+    </div>
+  ))}
+</div>
+
+						<button onClick={() => {
+							editExam();
+							
+						}}
+							className="rounded bg-blue-500 px-2 py-1 text-white hover:bg-blue-600"
 						>
-							Cancel
+							Save
 						</button>
 
+						<CancelButton onClick={resetInputs} />
+
+					</div>
+
+				</div>
+
+			)}
+
+			{/* floating form for viewing exam */}
+			{showForm && typeForm === FormTypes.VIEW_EXAM && selectedExam && (
+				<div className="fixed inset-0 flex items-center justify-center bg-black/70"
+					onClick={resetInputs}>
+					<div className="w-full max-w-md  max-h-[90vh] overflow-y-auto rounded-xl bg-white p-6 text-black shadow-2xl"
+						onClick={(e) => e.stopPropagation()}>
+						<h2 className="mb-4 text-2xl font-semibold">
+							{selectedExam.title}
+						</h2>
+
+						<p className="mt-2 text-sm leading-6 text-slate-600"><strong>Exam Date: </strong>{`${selectedExam.date.toLocaleDateString()} `}</p>
+						<p className="mt-2 text-sm leading-6 text-slate-600"><strong>Exam Time: </strong>{`${selectedExam.date.toLocaleTimeString()} `}</p>
+						<p className="mt-2 text-sm leading-6 text-slate-600"> <strong>Topic Progress: </strong>{`${calculateProgress(selectedExam.topics)}%`}</p>
+						<hr className="my-2 border-gray-500" />
+						<strong className="mt-2 text-sm bold leading-6 text-slate-600">{"TOPICS!"}</strong>
+						<div className="mt-4 space-y-2">
+							{selectedExam.topics.map((topic, index) => (
+								<CheckBox
+									topic={topic}
+									onToggle={() => toggleTopic(index)} />
+							))}
+						</div>
 					</div>
 
 				</div>
