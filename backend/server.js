0fdccda33eaec017ac8d9e4dafc874c4d32fba3f
HelixAthu <hathukoralla@gmail.com>
2026-06-02 15:08:17 +0530

    feat: backend server implemented

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Focus Path backend is running");
+});
+
+const PORT = process.env.PORT || 5500;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
